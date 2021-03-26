065bf32b6e0c1be628f219c05927f1a63ea02b10
Louis Hollingworth <louis@hollingworth.ch>
2021-03-26 20:09:33 +0000

    Allowed for better secrets handling (I'm lazy). Added commands helper and a test command.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-//const secrets = require('./secrets.json')
+const command = require('./helpers/commands')
+const config = require('./config.json')
+
+var token
+
+if (config.mode == "dev") {
+    const secrets = require('./secrets.json')
+    token = secrets.token
+} else {
+    token = process.env.DJS_TOKEN
+}
 
 client.on('ready', () => {
     console.log('Client is ready')
 
+    command(client, "ping", (message) => {
+        message.channel.send("Pong!")
+    })
 })
 
-client.login(process.env.DJS_TOKEN)
+client.login(token)
