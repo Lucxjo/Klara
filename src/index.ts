a6efd0e128068814c93f5c9c420a292bacc0dde4
Ludoviko <me@ludoviko.xyz>
2021-09-11 19:56:03 +0100

    Bot has first test command

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import DiscordJS, { Intents } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`The bot is ready!`);
+});
+
+client.login(process.env.DJS_TOKEN);
+client.on('messageCreate', (message) => {
+    if (message.content === `.ping`) {
+        message.reply({
+            content: `Pong!`,
+        });
+    }
+});
